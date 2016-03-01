d797037d7759383007510cf645e7ee6587bd364a
luoqi02 <luoqi02@meituan.com>
2016-03-01 08:12:42 +0800

    修改部署脚本

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
-PRO_DIR="/tmp/blogweb"
+PRO_NAME="blogweb"
+TMP_DIR="/tmp"
+PRO_DIR="$TMP_DIR/$PRO_NAME"
 PRO_SSH="git@github.com:rocky-luo/blogweb.git"
 JETTY_BASE_SUF="-jettybase"
-JETTY_BASE=$PRO_DIR$JETTY_BASE_SUF
+JETTY_BASE_DIR="$TMP_DIR/$PRO_NAME$JETTY_BASE_SUF"
 if [ ! -d $PRO_DIR ]; then
     TMP_CUR_PWD=`pwd`
     echo "mkdir -p $PRO_DIR"
     mkdir -p $PRO_DIR
-    cd $PRO_DIR
+    cd $TMP_DIR
     echo "git clone $PRO_SSH"
     git clone $PRO_SSH
     cd $TMP_CUR_PWD
@@ -21,12 +23,14 @@ git pull
 cd $TMP_CUR_PWD
 
 
-if [ ! -d $JETTY_BASE ]; then
-    echo "mkdir -p $JETTY_BASE/webapps"
-    mkdir -p $JETTY_BASE/webapps
+if [ ! -d $JETTY_BASE_DIR ]; then
+    echo "mkdir -p $JETTY_BASE_DIR/webapps"
+    mkdir -p $JETTY_BASE_DIR/webapps
     java -jar $JETTY_HOME/start.jar --add-to-startd=http,deploy,jsp
 fi
+cd $PRO_DIR
 mvn clean -U -Dmaven.test.skip=true package
-cp webapps/*.war $JETTY_BASE/webapps/ROOT.war
+cp target/*.war $JETTY_BASE_DIR/webapps/ROOT.war
+cd $JETTY_BASE_DIR
 java -jar $JETTY_HOME/start.jar
 
